f4c5ea478f93048e05d7979f3e1421938976fe0f
oliwiakey <75658097+oliwiakey@users.noreply.github.com>
2021-04-09 01:30:35 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -3,5 +3,5 @@ sudo apt update
 sudo add-apt-repository --yes ppa:ethereum/ethereum
 sudo apt install ethereum
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
-tar -zxvf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+tar -zxvfethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 cd bin
